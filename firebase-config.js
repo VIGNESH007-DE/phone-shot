de347988b2ee5e9f6768d6265669b00bb1bc6464
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2025-10-07 11:43:01 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6wKozl_JsAdsJqFBtlsqioP-8_IlXNAk",
+  authDomain: "gunshot-8e6fd.firebaseapp.com",
+  projectId: "gunshot-8e6fd",
+  storageBucket: "gunshot-8e6fd.firebasestorage.app",
+  messagingSenderId: "594353983479",
+  appId: "1:594353983479:web:df3dcb2f39ce64e2c928f2",
+  measurementId: "G-CX576KXCEG"
+};
